a1fe1e1ad493d726b92ca3ba1d7351c8d55caa82
isaiasaltoe <isaiasaltoe7@gmail.com>
2025-12-24 12:12:11 -0300

    fix: email error

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  # default from: "from@example.com"
+  # layout "mailer"
 
 
 
 
-  def welcome_email
-    @user = params[:user]
-    @url = "https:"
-
+  # def welcome_email
+  #   @user = params[:user]
+  #   @url = "https:"
+  # end  
 
 
 end
